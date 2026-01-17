c03d42edd2139691930dc3b4c94890b66775125b
Naitik <naitikjha1845@gmail.com>
2026-01-18 00:06:10 +0530

    cloud commit

--- networksecurity/pipelines/training_pipeline.py
+++ networksecurity/pipelines/training_pipeline.py
@@ -3,12 +3,12 @@ import sys
 
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
-
+from networksecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.model_trainer import ModelTrainer
-
+from networksecurity.cloud.s3_syncer import S3Sync
 from networksecurity.entity.config_entity import (
     TrainingPipelineConfig,
     DataIngestionConfig,
@@ -28,6 +28,7 @@ from networksecurity.entity.artifact_entity import (
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync=S3Sync()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
@@ -121,6 +122,25 @@ class TrainingPipeline:
 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+        
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    ## local final model is going to s3 bucket     
+        
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+            self.s3(folder = self.training_pipeline_config.model_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+
+
 
     def run_pipeline(self) -> ModelTrainerArtifact:
         try:
